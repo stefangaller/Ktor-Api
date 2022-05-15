15a02fc7bbeb1a4079bd2785c95823659bdec5dd
Jesus Ruiz <jesrui@posteo.net>
2022-05-15 15:00:36 +0200

    run cucumber tests using JUnit
    See
    https://www.baeldung.com/java-cucumber-gradle

--- src/test/kotlin/at/stefangaller/RunCucumberTest.kt
+++ src/test/kotlin/at/stefangaller/RunCucumberTest.kt
@@ -5,5 +5,5 @@ import io.cucumber.junit.CucumberOptions
 import org.junit.runner.RunWith
 
 @RunWith(Cucumber::class)
-@CucumberOptions(plugin = ["pretty"])
+@CucumberOptions(plugin = ["pretty"], features = ["classpath:features/"])
 class RunCucumberTest
