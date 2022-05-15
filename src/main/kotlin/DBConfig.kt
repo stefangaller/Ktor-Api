e5dd6594bee201fba56f1a448e173a264a3b34a9
Jesus Ruiz <jesrui@posteo.net>
2022-05-15 14:15:40 +0200

    remove unused annotation

--- src/main/kotlin/DBConfig.kt
+++ src/main/kotlin/DBConfig.kt
@@ -4,13 +4,10 @@ import at.stefangaller.data.Books
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.application.Application
-import io.ktor.util.KtorExperimentalAPI
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.LoggerFactory
-
-const val HIKARI_CONFIG_KEY = "ktor.hikariconfig"
 
 fun Application.initDB() {
     val configPath = "/dbconfig.properties"
